23e4776facfeb74ed47aeee50b61642842eec9b6
kabascolby <kabascolby@gmail.com>
2020-03-07 16:58:25 -0800

    establishing client and server communication

--- server/server.h
+++ server/server.h
@@ -1,4 +1,29 @@
-#ifdef SERVER_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkaba <lkaba@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/07 10:34:31 by lkaba             #+#    #+#             */
+/*   Updated: 2020/03/07 16:38:47 by lkaba            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_H
 # define SERVER_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <time.h>
+# define BUFSIZE 200
+
+struct sockaddr_in server_address;
 
 #endif
