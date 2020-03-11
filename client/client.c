02e3b50247f216f5ffbfc854f9c4f662d7970c74
kabascolby <kabascolby@gmail.com>
2020-03-10 20:15:12 -0700

    Cleaning up the mess client side before starting implementating the command indextable

--- client/client.c
+++ client/client.c
@@ -6,49 +6,58 @@
 /*   By: lkaba <lkaba@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 15:46:52 by lkaba             #+#    #+#             */
-/*   Updated: 2020/03/07 18:40:57 by lkaba            ###   ########.fr       */
+/*   Updated: 2020/03/10 20:06:13 by lkaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "client.h"
 
-int main(int argc,char **argv)
+static int	check_port(uint16_t port)
 {
-    int     socket_fd,n;
-    int     status;
-    char    sendline[BUFSIZE];
-    char    recvline[BUFSIZE];
+	if (port < 1024 || port > 65535)
+	{
+		fprintf(stderr, "Invalid port, enter (1024 - 65535.)\n");
+		return (1);
+	}
+	return (0);
+}
 
-    if (argc != 2 || (argc == 2 && (atoi(argv[1]) < 1024 || atoi(argv[1]) > 65535)))
-    {
-        fprintf(stderr, "Invalid port, enter (1024 - 65535.)\n");
-        return (-1);
-    }
+uint8_t		create_connection(t_config *config, int port)
+{
+	struct sockaddr_in	server_address;
+	struct hostent		*host_entry;
 
-	/*Create a socker fd*/
-    socket_fd = socket(AF_INET,SOCK_STREAM,0);
+	if(check_port(port) || !strlen(SERVERIP))
+		return (1);
 
-    bzero(&server_address,sizeof server_address);
-    server_address.sin_family=AF_INET;
-    server_address.sin_port=htons(atoi(argv[1]));
-	inet_aton("10.113.100.129", &server_address.sin_addr);
-    // server_address.sin_addr.s_addr = INADDR_ANY;
+	bzero(&server_address, sizeof(server_address));
 
-    status = connect(socket_fd,(struct sockaddr *)&server_address,sizeof(server_address));
-    if (status == -1)
-    {
-        fprintf(stderr, "Connection error\n");
-        return 0;
-    }
-    while(1)
-    {
-        bzero(sendline, BUFSIZE);
-        bzero(recvline, BUFSIZE);
-        fgets(sendline, BUFSIZE, stdin);
-        write(socket_fd, sendline, strlen(sendline)+1);
-        read(socket_fd, recvline, 100);
-        printf("\033[1;34mServer ->\033[m %s",recvline);
-    }
-    return (0);
+	/* put host's address in socket structure */
+	// inet_aton(SERVERIP, &server_address.sin_addr);
+	server_address.sin_addr.s_addr = inet_addr(SERVERIP);
+
+
+	/* put host's address family and the port in socket structure */
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(port);
+
+	/*Create a file descriptor by defining socket characteristique*/
+	if ((config->socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		fprintf(stderr, "can't open socket");
+		return (1);
+	}
+	/* Establish a connection */
+	if((config->status = connect(config->socket_fd,
+								(struct sockaddr *)&server_address,
+								sizeof(server_address)))
+	< 0)
+	{
+		fprintf(stderr, "can't connect to server");
+		return (1);
+	}
+	config->port = port;
+	strcpy(config->server_name, "Minaserver");
+	return (0);
 }
--- client/requestHandler.c
+++ client/requestHandler.c
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.h                                           :+:      :+:    :+:   */
+/*   requestHandler.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkaba <lkaba@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/07 10:34:31 by lkaba             #+#    #+#             */
-/*   Updated: 2020/03/08 09:25:26 by lkaba            ###   ########.fr       */
+/*   Created: 2020/03/10 20:05:20 by lkaba             #+#    #+#             */
+/*   Updated: 2020/03/10 20:07:52 by lkaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERVER_H
-# define SERVER_H
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <sys/types.h>
-# include <sys/socket.h>
-# include <netdb.h>
-# include <netinet/in.h>
-# include <arpa/inet.h>
-# include <time.h>
-# include <stdarg.h>
-# define BUFSIZE 200
-
-typedef struct s_req		t_req;
-
-struct sockaddr_in server_address;
-
-#endif
+#include "client.h"
